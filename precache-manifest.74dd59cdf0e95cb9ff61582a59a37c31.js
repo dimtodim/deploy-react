78fb81c870534502f78ba21b11f8a5b19bbc18ae
Your Namedimtodim <dimtodim@gmail.com>
2019-08-11 09:48:26 +0200

    Updates

---
+++ precache-manifest.74dd59cdf0e95cb9ff61582a59a37c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad646681f38ee40ef4211b272dcf5808",
+    "url": "/deploy-react.git/index.html"
+  },
+  {
+    "revision": "7591937b7f788f7c2806",
+    "url": "/deploy-react.git/static/css/main.ea39107a.chunk.css"
+  },
+  {
+    "revision": "4995a715667a29bd302a",
+    "url": "/deploy-react.git/static/js/2.9ccba488.chunk.js"
+  },
+  {
+    "revision": "7591937b7f788f7c2806",
+    "url": "/deploy-react.git/static/js/main.faf1e419.chunk.js"
+  },
+  {
+    "revision": "7de78e6be20a1f3418f5",
+    "url": "/deploy-react.git/static/js/runtime~main.c4ba443a.js"
+  }
+]);
